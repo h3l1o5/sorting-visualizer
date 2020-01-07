30b5fb1df215a936b1118bd59e4639900d085c51
h3l1o5 <gguang.sun@gmail.com>
2020-01-07 22:37:13 +0800

    data

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,58 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
+import _ from "lodash";
 
 const App: React.FC = () => {
+  const [state, setState] = useState({
+    max: 20,
+    elements: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
+  });
+
+  useEffect(() => {
+    setInterval(() => {
+      const newElements = _.range(20).map(() => Math.floor(Math.random() * 49) + 1);
+      const newMax = _.max(newElements) || 0;
+
+      setState({
+        max: newMax,
+        elements: newElements,
+      });
+    }, 2000);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+    <div style={{ width: "100vw", height: "100vh", display: "flex", flexDirection: "column" }}>
+      <div style={{ flex: 1, backgroundColor: "red" }}>TOP</div>
+      <div
+        style={{
+          flex: 5,
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+          alignItems: "flex-end",
+          padding: "20px 40px",
+        }}
+      >
+        {state.elements.map((e, index) => (
+          <div
+            key={index}
+            style={{
+              flex: 1,
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "flex-start",
+              alignItems: "center",
+              height: `calc(100% * ${e / state.max})`,
+              margin: "0px 5px",
+              backgroundColor: "#fff",
+              borderRadius: "5px",
+            }}
+          >
+            {e}
+          </div>
+        ))}
+      </div>
+      <div style={{ flex: 2, backgroundColor: "red" }}>BOTTOM</div>
     </div>
   );
 };
